d59f4e2f05edf0f11d846997de7e6f0f1d6b1851
midseung <sintjd103@nave.rcom>
2025-03-20 10:49:36 +0900

    3.19

--- src/main/java/com/spacedong/mapper/ReservationMapper.java
+++ src/main/java/com/spacedong/mapper/ReservationMapper.java
@@ -18,9 +18,9 @@ public interface ReservationMapper {
     void createReservation(ReservationBean reservationBean);
 
     //예약 번호 조회
-    @Select("select reservation_id from reservation where member_id = #{memberId} and item_id = #{itemId} and start_time = #{startTime} and reservation_date = #{reservationDate}")
+    @Select("select reservation_id from reservation where member_id = #{memberId} and item_id = #{itemId} and start_time = #{startTime} and reservation_date = #{reservationDate} and status = 'CONFIRMED'")
     int getReservationId(@Param("memberId") String memberId, @Param("itemId") String itemId, @Param("startTime") int startTime, @Param("reservationDate") Date reservationDate);
-    
+
     // 특정 날짜에 예약된 시간대 조회 - 쉼표 추가
     @Select("SELECT DISTINCT r.item_id ,r.start_time, r.end_time " +
             "FROM reservation r " +
