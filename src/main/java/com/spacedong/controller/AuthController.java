758049166039d8402c37ed460d2b8577233052f0
KIMYONGSEOK1 <dydtjr12300@gmail.com>
2025-04-01 12:27:29 +0900

    김용석 0401

--- src/main/java/com/spacedong/controller/AuthController.java
+++ src/main/java/com/spacedong/controller/AuthController.java
@@ -10,19 +10,19 @@ public class AuthController {
     @PostMapping(value = "/sendCode", produces = "text/plain;charset=UTF-8")
     @ResponseBody
     public String sendAuthCode(@RequestParam String phoneNumber) {
-//        try {
-//            SmsAuthService.sendAuthCode(phoneNumber);
-//            return "인증번호가 발송되었습니다.";
-//        } catch (Exception e) {
-//            return "인증번호 발송에 실패했습니다: " + e.getMessage();
-//        }
-           return "인증번호가 발송되었습니다.";
+        try {
+            SmsAuthService.sendAuthCode(phoneNumber);
+            return "인증번호가 발송되었습니다.";
+        } catch (Exception e) {
+            return "인증번호 발송에 실패했습니다: " + e.getMessage();
+        }
+//           return "인증번호가 발송되었습니다.";
     }
 
     // 인증번호 검증 API
     @PostMapping("/verifyCode")
     public boolean verifyAuthCode(@RequestParam String phoneNumber, @RequestParam String authCode) {
-//        return SmsAuthService.verifyAuthCode(phoneNumber, authCode);
-        return true;
+        return SmsAuthService.verifyAuthCode(phoneNumber, authCode);
+//        return true;
     }
 }
